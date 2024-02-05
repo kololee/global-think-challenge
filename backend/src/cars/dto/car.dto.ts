6f304472c69ec20b1078fad827b28dd0fc8aba0c
Federico Schmitt <kolo.schmitt@gmail.com>
2024-02-05 19:25:41 -0300

    add ValidationPipe and some class-validators

--- backend/src/cars/dto/car.dto.ts
+++ backend/src/cars/dto/car.dto.ts
@@ -1,17 +1,68 @@
 import { EngineType } from '../car.entity';
+import {
+  IsNumber,
+  IsString,
+  IsEnum,
+  IsNotEmpty,
+  MinLength,
+  Min,
+  Max,
+  IsOptional,
+} from 'class-validator';
 
 export class CreateCarDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   brand: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   version: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   model: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(1900)
+  @Max(new Date().getFullYear())
   year: number;
+
+  @IsEnum(EngineType)
+  @IsNotEmpty()
   engineType: EngineType;
 }
 
 export class UpdateCarDto {
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MinLength(3)
   brand?: string;
+
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MinLength(3)
   version?: string;
+
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MinLength(3)
   model?: string;
+
+  @IsNumber()
+  @IsOptional()
+  @Min(1900)
+  @Max(new Date().getFullYear())
   year?: number;
+
+  @IsEnum(EngineType)
+  @IsOptional()
   engineType?: EngineType;
 }
